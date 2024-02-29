5d2e1a441618aef67275ef06164fa2c3e52bbaeb
Mehrnaz <mehrnazhemmat1998@gmail.com>
2024-02-29 23:09:52 +0330

    Test

---
+++ note-app/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="container">
+      <div className="note-header">Note header</div>
+      <div className="note-app">
+        <div className="add-new-note">Add new note form</div>
+        <div className="note-container"> My Notes </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
