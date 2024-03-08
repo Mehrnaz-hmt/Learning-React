810d6e47d6b4b55d5c88f77305be0df4be4e5f99
Mehrnaz <mehrnazhemmat1998@gmail.com>
2024-03-08 20:02:43 +0330

    S02-E28Contrlolled Component in React.js

--- note-app/src/components/AddNewNote.jsx
+++ note-app/src/components/AddNewNote.jsx
@@ -1,20 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function AddNewNote() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
   const handleSubmit = (e) => {
-    console.log(e)
-    e.preventDefault()
-  }
+    console.log(e);
+    e.preventDefault();
+    const newNote = {
+      title,
+      description,
+      id: Date.now(),
+      completed: false,
+      createAt: new Date().toDateString(),
+    };
+    setTitle("");
+    setDescription("");
+    console.log(newNote);
+
+    //We should update our notes in the next Step!
+  };
 
   return (
     <div>
       <h2>Add new note</h2>
 
-      <form onClick={handleSubmit} className="note-form">
-        <input type="text" className="text-field" placeholder="Note Title"/>
-        <input type="text" className="text-field"  placeholder="Add escription..."/>
-        <button type="submit"  className="btn btn--primary">Add New Note</button>
+      <form onSubmit={handleSubmit} className="note-form">
+        <input
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="Note Title"
+        />
+        <input
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          type="text"
+          className="text-field"
+          placeholder="Add escription..."
+        />
+        <button type="submit" className="btn btn--primary">
+          Add New Note
+        </button>
       </form>
     </div>
   );
