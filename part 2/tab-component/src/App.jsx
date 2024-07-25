6550c7bfb2c99a9232116ce97019f8dfc2870384
Mehrnaz <mehrnazhemmat1998@gmail.com>
2024-07-26 00:25:35 +0330

    start again

--- part 2/tab-component/src/App.jsx
+++ part 2/tab-component/src/App.jsx
@@ -24,7 +24,7 @@ const tabData = [
 function App() {
   const [activeTab, setActiveTab] = useState(2); //[state,setState]
   const [isOpen, setIsOpen] = useState(true);
-  const [counter,setCount] = useState(0)
+  const [counter, setCount] = useState(0);
 
   const handleActiveTab = (id) => {
     setActiveTab(id);
@@ -33,39 +33,38 @@ function App() {
   };
 
   const handleOpen = () => {
-    setIsOpen((isOpen) => !isOpen );
-   
+    setIsOpen((isOpen) => !isOpen);
   };
 
   return (
     <div>
       <div>
-        <button className="closeButton" onClick={() => handleOpen(!isOpen)}> ❌ </button>
-            {
-            isOpen ? (
-              <div className="tab">
-                <div className="tab__header">
-                  {tabData.map((tab) => (
-                    <button
-                      onClick={() => handleActiveTab(tab.id)}
-                      key={tab.id}
-                      className={activeTab === tab.id ? "active" : ""}
-                    >
-                      <span>{tab.title}</span>
-                      <span className="tab-indicator"></span>
-                    </button>
-                  ))}
-                </div>
+        <button className="closeButton" onClick={() => handleOpen(!isOpen)}>
+          {" "}
+          ❌{" "}
+        </button>
+        {isOpen ? (
+          <div className="tab">
+            <div className="tab__header">
+              {tabData.map((tab) => (
+                <button
+                  onClick={() => handleActiveTab(tab.id)}
+                  key={tab.id}
+                  className={activeTab === tab.id ? "active" : ""}
+                >
+                  <span>{tab.title}</span>
+                  <span className="tab-indicator"></span>
+                </button>
+              ))}
+            </div>
 
-                <div className="tab__content">
-                  {tabData[activeTab - 1].content} - {counter}
-                </div>
-              </div>
-            ) : (
-              <p>Cloced</p>
-            )
-          } 
-      
+            <div className="tab__content">
+              {tabData[activeTab - 1].content} - {counter}
+            </div>
+          </div>
+        ) : (
+          <p>Cloced</p>
+        )}
       </div>
     </div>
   );
